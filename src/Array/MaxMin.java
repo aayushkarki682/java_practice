ff6ba8a99a01d74f8e94356389751480342d0cb0
Aayush Karki <ayushkarki12345@gmail.com>
2021-08-31 16:47:31 -0500

    MaxMin done

--- src/Array/MaxMin.java
+++ src/Array/MaxMin.java
@@ -1,4 +1,16 @@
 package Array;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class MaxMin {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        int max = Arrays.stream(arr).max().getAsInt();
+        int min = Arrays.stream(arr).min().getAsInt();
+
+        System.out.println(max);
+        System.out.println(min);
+    }
 }
